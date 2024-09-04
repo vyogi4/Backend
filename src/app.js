f5631c6fd6bb9b0a5fa449ebb8ea1a0f558cef89
vyogi4 <vyogi4151@gmail.com>
2024-09-04 20:37:11 +0530

    add structure changes

--- src/app.js
+++ src/app.js
@@ -1,31 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // this is how you can create multiple files in a folder using terminal
 
 // foreach ($file in "app.js","constants.js","index.js"){
