f109702533c36365f17cb26c1ed1b3ab966f5dab
vyogi4 <vyogi4151@gmail.com>
2024-09-04 15:32:46 +0530

    setup project files

---
+++ src/app.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// this is how you can create multiple files in a folder using terminal
+
+// foreach ($file in "app.js","constants.js","index.js"){
+//     ni $file -itemType File
+// }
